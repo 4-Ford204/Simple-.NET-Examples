326e51a7b7acd830e4e99f66fcef855bedb565ef
Ford <huynhduy20042002@gmail.com>
2024-05-30 11:29:34 +0700

    RabbitMQ

--- RabbitMQ/Publisher/Program.cs
+++ RabbitMQ/Publisher/Program.cs
@@ -1,24 +1,37 @@
 ﻿using RabbitMQ.Client;
+using System;
 using System.Text;
 
 namespace RabbitMQ.Publisher
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using var connection = factory.CreateConnection();
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare("RabbitMQ-Queue", false, false, false, null);
-                string message = "creating a message using asp.net core rabbitmq long ngu 123";
-                var body = Encoding.UTF8.GetBytes(message);
-                channel.BasicPublish("", "Test", null, body);
-                Console.WriteLine("sent message:{0}", message);
-            }
+            using var channel = connection.CreateModel();
 
-            Console.ReadLine();
+            channel.QueueDeclare("RabbitMQ-Queue", false, false, false, null);
+            // [Exchange Type] direct, fanout, topic, or headers
+            channel.ExchangeDeclare("Logs", "topic", false, false, null);
+
+            string message = "Sending a message using ASP.NET Core RabbitMQ";
+            byte[] body;
+
+            do
+            {
+                body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "RabbitMQ-Queue", null, body);
+                channel.BasicPublish("Logs", "Information.General", null, Encoding.UTF8.GetBytes($"[Information] {message}"));
+                channel.BasicPublish("Logs", "Warning.Minor", null, Encoding.UTF8.GetBytes($"[Warning] {message}"));
+                channel.BasicPublish("Logs", "Error.Important", null, Encoding.UTF8.GetBytes($"[Error] {message}"));
+
+                Console.WriteLine($"Publisher: \t {message} [{DateTime.Now}]");
+                Console.Write("Enter message: \t ");
+            }
+            while (!string.IsNullOrEmpty(message = Console.ReadLine() ?? throw new Exception("STOP")));
         }
     }
 }
--- RabbitMQ/Subscriber/Program.cs
+++ RabbitMQ/Subscriber/Program.cs
@@ -1,30 +1,37 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System;
 using System.Text;
 
 namespace RabbitMQ.Subscriber
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("RabbitMQ-Queue", false, false, false, null);
+            //channel.ExchangeDeclare("Logs", "topic", false, false, null);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queueName, "Logs", "Information.General");
+            channel.QueueBind(queueName, "Logs", "*.Minor");
+            channel.QueueBind(queueName, "Logs", "Error.*");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
             {
-                channel.QueueDeclare("Test", false, false, false, null);
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var messagebody = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(messagebody);
-                    Console.WriteLine("Recieved message:{0}", message);
-                };
+                var messageBody = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(messageBody);
+                Console.WriteLine($"Subscriber: {message}");
+            };
+            channel.BasicConsume("RabbitMQ-Queue", true, consumer);
+            channel.BasicConsume(queueName, true, consumer);
 
-                channel.BasicConsume("Test", true, consumer);
-
-                Console.ReadLine();
-            }
+            while (true) { }
         }
     }
 }
