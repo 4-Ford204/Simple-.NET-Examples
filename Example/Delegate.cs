6b2774ec26c5570c692f3a2cda72ad93ce63a71a
Ford <huynhduy20042002@gmail.com>
2024-08-13 14:30:30 +0700

    Xử lý sự kiện với delegate

--- Example/Delegate.cs
+++ Example/Delegate.cs
@@ -5,6 +5,7 @@ namespace Example
     public class Delegate
     {
         delegate void Operation(float a, float b);
+        delegate void EventHandler(string message);
 
         public void DelegateExample()
         {
@@ -24,6 +25,12 @@ namespace Example
             operation(2, 0);
 
             LaunchOperation(operation);
+
+            EventPublisher eventPublisher = new EventPublisher();
+            EventSubscriber eventSubscriber = new EventSubscriber();
+
+            eventPublisher.OnMessageSent += eventSubscriber.HandleMessage;
+            eventPublisher.SendMessage("DELEGATE");
         }
 
         private void Addition(float a, float b)
@@ -52,6 +59,26 @@ namespace Example
         private void LaunchOperation(Operation operation)
         {
             operation(5, 2);
+        }
+
+        class EventPublisher
+        {
+            public event EventHandler OnMessageSent;
+
+            public void SendMessage(string message)
+            {
+                Console.WriteLine($"Sending message: {message}");
+
+                OnMessageSent?.Invoke(message);
+            }
+        }
+
+        class EventSubscriber
+        {
+            public void HandleMessage(string message)
+            {
+                Console.WriteLine($"Received message: {message}");
+            }
         }
     }
 }
