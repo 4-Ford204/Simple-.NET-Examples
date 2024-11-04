8479948dfaad829e437ccde0d9db0b12fe6ae3ac
Ford <huynhduy20042002@gmail.com>
2024-11-04 10:58:46 +0700

    Bubble Sort

---
+++ Example/Algorithms/Sorting.cs
@@ -0,0 +1,26 @@
+﻿namespace Example.Algorithms
+{
+    public class Sorting
+    {
+        public int[] BubbleSort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool swap = false;
+
+                for (int j = 0; j < nums.Length - i - 1; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        (nums[j + 1], nums[j]) = (nums[j], nums[j + 1]);
+                        swap = true;
+                    }
+                }
+
+                if (!swap) break;
+            }
+
+            return nums;
+        }
+    }
+}
