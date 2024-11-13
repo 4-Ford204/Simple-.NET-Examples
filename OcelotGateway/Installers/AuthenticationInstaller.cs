030760ae06bd427103131f10aa7505a1fe952123
Ford <huynhduy20042002@gmail.com>
2024-11-13 15:47:03 +0700

    Ocelot Gateway - Authentication

---
+++ OcelotGateway/Installers/AuthenticationInstaller.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace OcelotGateway.Installers
+{
+    public class AuthenticationInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateLifetime = true,
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTConfiguration:Key"] ?? "")),
+                        ValidateIssuerSigningKey = true
+                    };
+                });
+        }
+
+        public void ConfigureRequestPiplineAsync(WebApplication app)
+        {
+            app.UseAuthentication();
+        }
+    }
+}
