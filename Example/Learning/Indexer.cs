87f613113a4551b8e472b322f1b36d94794c2baf
Ford <huynhduy20042002@gmail.com>
2024-12-20 11:36:27 +0700

    Indexer

---
+++ Example/Learning/Indexer.cs
@@ -0,0 +1,36 @@
+﻿namespace Example.Learning
+{
+    public class Indexer
+    {
+        public static int Length { get; set; } = 'z' - 'a' + 1;
+        private char[] Chars { get; set; } = new char[Length];
+
+        public Indexer()
+        {
+            for (int i = 0; i < Length; i++) Chars[i] = (char)('a' + i);
+        }
+
+        public char this[int index]
+        {
+            get => (index < 0 || index >= Length) ? ' ' : Chars[index];
+
+            set
+            {
+                if (index >= 0 && index < Length) Chars[index] = value;
+            }
+        }
+
+        public int this[char value]
+        {
+            get
+            {
+                int index = Length;
+
+                while (--index > 0)
+                    if (Chars[index] == value) return index;
+
+                return index;
+            }
+        }
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -1,4 +1,5 @@
-﻿using Example.Algorithms;
+﻿using Example.Learning;
+using System;
 
 namespace Example
 {
@@ -6,7 +7,17 @@ namespace Example
     {
         public static void Main(string[] args)
         {
-            SortingExtension.RunBenchmark();
+            var indexer = new Indexer();
+
+            for (int i = 0; i < Indexer.Length; i++)
+                Console.Write($"{indexer[i]} ");
+
+            Console.WriteLine();
+
+            for (int i = 0; i < Indexer.Length; i++)
+                Console.Write($"{indexer[(char)('a' + i)]} ");
+
+            Console.ReadKey();
         }
     }
 }
