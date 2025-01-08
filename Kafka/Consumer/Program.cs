827945f7a0efa38a08e81319743e3f50863c71d7
Ford <huynhduy20042002@gmail.com>
2025-01-08 15:24:31 +0700

    Kafka

---
+++ Kafka/Consumer/Program.cs
@@ -0,0 +1,54 @@
+﻿using Confluent.Kafka;
+
+namespace Consumer
+{
+    public class Program
+    {
+        private static string BootstrapServers { get; set; } = "localhost:9092";
+        private static string Topic { get; set; } = "kafka-topic";
+        private static string ClientId { get; set; } = "kafka-client-id";
+        private static string GroupId { get; set; } = "kafka-group-id";
+
+        public static void Main(string[] args)
+        {
+            ConsumeMessage();
+        }
+
+        public static void ConsumeMessage()
+        {
+            var configuration = new ConsumerConfig
+            {
+                BootstrapServers = BootstrapServers,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                ClientId = ClientId,
+                GroupId = GroupId,
+                BrokerAddressFamily = BrokerAddressFamily.V4
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(configuration).Build())
+            {
+                consumer.Subscribe(Topic);
+
+                try
+                {
+                    while (true)
+                    {
+                        var consumeMessage = consumer.Consume();
+
+                        Console.WriteLine($"Consume message '{consumeMessage.Message.Value}' <- {consumeMessage.TopicPartitionOffset}");
+                    }
+                }
+                catch (OperationCanceledException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    consumer.Close();
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Kafka/Producer/Program.cs
@@ -0,0 +1,70 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+
+namespace Producer
+{
+    public class Program
+    {
+        private static string BootstrapServers { get; set; } = "localhost:9092";
+        private static string Topic { get; set; } = "kafka-topic";
+        private static string ClientId { get; set; } = "kafka-client-id";
+
+        public static void Main(string[] args)
+        {
+            GetKafka().Wait();
+            ProduceMessage().Wait();
+        }
+
+        public static async Task GetKafka()
+        {
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = BootstrapServers }).Build())
+            {
+                try
+                {
+                    await adminClient.CreateTopicsAsync(
+                        [
+                            new TopicSpecification {
+                                Name = Topic,
+                                ReplicationFactor = 1,
+                                NumPartitions = 1
+                            }
+                        ]
+                    );
+                }
+                catch (CreateTopicsException e)
+                {
+                    Console.WriteLine($"{e.Results[0].Topic}: {e.Results[0].Error.Reason}");
+                }
+            }
+        }
+
+        public static async Task ProduceMessage()
+        {
+            var configuration = new ProducerConfig
+            {
+                BootstrapServers = BootstrapServers,
+                ClientId = ClientId,
+                BrokerAddressFamily = BrokerAddressFamily.V4
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(configuration).Build())
+            {
+                string? input;
+                Console.WriteLine("Enter message: ");
+
+                while (!String.IsNullOrEmpty(input = Console.ReadLine()))
+                {
+                    var message = new Message<Null, string>
+                    {
+                        Value = input
+                    };
+                    var produceMessage = await producer.ProduceAsync(Topic, message);
+
+                    Console.WriteLine($"Produce message '{message}' -> {produceMessage.TopicPartitionOffset}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
